6281862674d1a8458a0dcf99dce01da570fb0775
AbelDawson <abeldattsun@gmail.com>
2020-09-17 22:54:32 -0400

    changed apt-install discord to snap install discord

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install git && sudo apt-get install vim && sudo apt-get install i3-wm && sudo apt-get install i3status && sudo apt-get install rofi && sudo apt-get install htop && sudo apt-get install compton && sudo apt-get install discord
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install git && sudo apt-get install vim && sudo apt-get install i3-wm && sudo apt-get install i3status && sudo apt-get install rofi && sudo apt-get install htop && sudo apt-get install compton 
+
+sudo snap install discord
 
 cp config ~/.config/i3
 cp vimrc ~
