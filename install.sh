710b2d9021976aca11aece4ff4514608f7646cc9
AbelDawson <abeldattsun@gmail.com>
2020-09-17 22:50:34 -0400

    fixed syntex issue with .config

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install git && sudo apt-get install vim && sudo apt-get install i3wm && sudo apt-get install i3status && sudo apt-get install rofi && sudo apt-get install htop && sudo apt-get install compton
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install git && sudo apt-get install vim && sudo apt-get install i3wm && sudo apt-get install i3status && sudo apt-get install rofi && sudo apt-get install htop && sudo apt-get install compton && sudo apt-get install discord
 
-cp config ~/config/i3
+cp config ~/.config/i3
 cp vimrc ~
 mv ~/vimrc ~/.vimrc
+cp bear.jpg ~/Pictures
 
