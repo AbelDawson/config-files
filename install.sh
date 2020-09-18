cd302d91a09e0acda614b71dcec2fc47b0846436
AbelDawson <abeldattsun@gmail.com>
2020-09-17 22:42:04 -0400

    added normal background

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install git && sudo apt-get install vim && sudo apt-get install i3wm && sudo apt-get install i3status && sudo apt-get install rofi && sudo apt-get install htop
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install git && sudo apt-get install vim && sudo apt-get install i3wm && sudo apt-get install i3status && sudo apt-get install rofi && sudo apt-get install htop && sudo apt-get install compton
 
 cp config ~/config/i3
 cp vimrc ~
