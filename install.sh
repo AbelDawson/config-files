9ce169e39da4d217ed951b45f2dafb72b1645bf8
AbelDawson <abeldattsun@gmail.com>
2020-09-18 20:15:10 -0400

    small change

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install git && sudo apt-get install vim && sudo apt-get install i3-wm && sudo apt-get install i3status && sudo apt-get install rofi && sudo apt-get install htop && sudo apt-get install compton && sudo apt-get install curl && sudo apt install feh && sudo apt-get install terminator   
+sudo pacman -Syu git vim i3-wm i3status-rist rofi feh terminator picom
 
-sudo snap install discord
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -28,3 +27,5 @@ echo cp terminator-config ~/.config/terminator
 cp terminator-config ~/.config/terminator
 echo mv ~/.config/terminator/terminator-config ~/.confgi/terminator/config
 mv ~/.config/terminator/terminator-config ~/.config/terminator/config
+echo cp status.toml ~/.config/i3
+cp status.toml ~/.config/i3
